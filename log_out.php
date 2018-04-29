4ba64c8a584cbfcd8ee2990404c289285cf611bc
DonVu <don.vu1212@gmail.com>
2018-04-28 23:36:20 -0700

    Implement logging out functionality

---
+++ log_out.php
@@ -0,0 +1,33 @@
+<?php
+  session_start();
+  if (empty($_SESSION)) {
+    echo "<script>window.location = history.back();</script>"; // go back to the previous page
+  }
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Welcome to Basketball Roster Project</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+  <?php
+    session_destroy();
+
+    include_once "default_navbar.php";
+  ?>
+
+  <div class="header">
+    Logged out
+  </div>
+  <div class="container">
+    <h2>You are now successfully logged out of Basketball Roster Project.</h2>
+    Didn't mean to logout? <a href="index.php">Login again</a>
+  </div>
+
+
+</body>
+</html>
--- logged_navbar.php
+++ logged_navbar.php
@@ -2,5 +2,5 @@
   <li><a href='index.php'>Home</a></li>
   <li><a href='about.php'>About</a></li>
   <li><a href='profile.php'>View Profile</a></li>
-  <li><a href='about.php'>Log Out</a></li>
+  <li><a href='log_out.php'>Log Out</a></li>
 </ul>
