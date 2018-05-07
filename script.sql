21017cb004bf2ea6a03efef0d60e3cf760f1dd8f
Suchartee Kitisopakul <suchartee.k@gmail.com>
2018-05-06 17:09:08 -0700

    script.sql
    This one is for testing player.php and coach.php
    
    This script is  for us to put into the database

---
+++ script.sql
@@ -0,0 +1,32 @@
+INSERT INTO Team (TeamName) VALUES 
+('Lakers'),
+('Golden State Warrior'),
+('Titan CSUF Basketball'),
+('Bruins UCLA Basketball')
+;
+
+INSERT INTO Player (FirstName, LastName, TeamID) VALUES
+('Travis', 'Wear', 1),
+('Lonzo', 'Ball', 1),
+('Brook', 'Lopez', 1),
+('Alex', 'Caruso', 1),
+('Nick', 'Young', 2),
+('David', 'West', 2),
+('Quinn', 'Cook', 2),
+('Alice', 'Kiti', 2),
+('Su', 'Htet', 3),
+('Don', 'Vu', 3),
+('Random', 'Person', 3),
+('May', 'White', 3),
+('Julie', 'Kim', 4),
+('Nancy', 'Smith', 4),
+('John', 'Doe', 4),
+('Alex', 'Nguyen', 4)
+;
+
+INSERT INTO Coach (FirstName, LastName, TeamID) VALUES
+('Luke', 'Walton', 1),
+('Steve', 'Kerr', 2),
+('Tom', 'Bettens', 3),
+('Somename', 'Somefamily', 4)
+;
