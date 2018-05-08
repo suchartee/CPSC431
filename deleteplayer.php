f8214c23f95d42f46108577d2eb56a23e924dc37
Suchartee Kitisopakul <suchartee.k@gmail.com>
2018-05-08 14:57:17 -0700

    delete player

--- deleteplayer.php
+++ deleteplayer.php
@@ -12,7 +12,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>Modify Player Page</title>
+  <title>Delete Player Page</title>
   <link rel="stylesheet" href="style.css">
 </head>
 
@@ -28,7 +28,7 @@
       $stmt->bind_result($playerID, $firstname, $lastname, $teamid, $teamname);
     }
     $stmt->data_seek(0);
-    echo "<div class=\"header\" style=\"display:table;\">Modify Player</div>
+    echo "<div class=\"header\" style=\"display:table;\">Delete Player</div>
           <div class=\"container\">
           <table>
             <tr>
@@ -38,21 +38,33 @@
               <th>Team Name</th>
               <th>Action</th>
             </tr>";
-
+            $row = array();
         while( $stmt->fetch() ) {
-          $row = array('id'=>$count++, 'playerid'=>$playerID, 'firstname'=>$firstname, 'lastname'=>$lastname, 'teamid'=>$teamid,'teamname'=>$teamname);
-          echo "<tr>
-
-            <td>". $row['id'] ."</td>
-            <td>". $row['firstname'] ."</td>
-            <td>". $row['lastname'] ."</td>
-            <td>". $row['teamname'] ."</td>
-            <td><a href=\"changeplayerinfo.php?playerid=".$row['playerid']."&firstname=".$row['firstname']."&lastname=".$row['lastname']."&teamid=".$row['teamid']."&teamname=".$row['teamname']."\">Change</a>
-          </tr>";
+            $row = array('id'=>$count++, 'playerid'=>$playerID, 'firstname'=>$firstname, 'lastname'=>$lastname, 'teamname'=>$teamname);
+              echo "<tr>
+                <td>". $row['id'] ."</td>
+                <td>". $row['firstname'] ."</td>
+                <td>". $row['lastname'] ."</td>
+                <td>". $row['teamname'] ."</td>
+                <td><a href=\"deleteplayer.php?playerid=".$row['playerid']."\">Delete</a>
+              </tr>";
         }
     echo "</table>
     </div>";
     ?>
+
+    <?php
+      if (isset($_GET["playerid"]) && !empty($_GET["playerid"])) {
+        $playerID = $_GET["playerid"];
+        $query = "DELETE FROM Player WHERE ID = ?";
+        if ($stmt = $db->prepare($query)) {
+          $stmt->bind_param("i", $playerID);
+          $stmt->execute();
+        }
+        echo '<script type="text/javascript"> alert("You have successfully deleted this player!")</script>';
+        echo "<script>window.location = 'deleteplayer.php';</script>";
+      }
+     ?>
 
 
 </body>
